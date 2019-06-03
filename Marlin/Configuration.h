2ed11dd7fe9476663dfc5495a8c995b15f55b4b1
xxorza@gmail.com <xxorza@gmail.com>
2019-06-02 20:44:00 -0700

    default jerk increased

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -749,12 +749,12 @@
  * value set here, it may happen instantaneously.
  */
 #if DISABLED(JUNCTION_DEVIATION)
-  #define DEFAULT_XJERK 5.0
-  #define DEFAULT_YJERK 5.0
+  #define DEFAULT_XJERK 10.0
+  #define DEFAULT_YJERK 10.0
   #define DEFAULT_ZJERK 0.3
 #endif
 
-#define DEFAULT_EJERK    5.0  // May be used by Linear Advance
+#define DEFAULT_EJERK    50.0  // May be used by Linear Advance
 
 /**
  * S-Curve Acceleration
