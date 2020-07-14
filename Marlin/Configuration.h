b924b61be71e29955e7cffb307d69e75bb4e5665
xxorza <xxorza@gmail.com>
2020-07-14 00:44:46 -0700

    fixed build

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2208,7 +2208,7 @@
 #endif
 
 // Support for Adafruit Neopixel LED driver
-#define NEOPIXEL_LED
+//#define NEOPIXEL_LED
 #if ENABLED(NEOPIXEL_LED)
   #define NEOPIXEL_TYPE   NEO_GRBW // NEO_GRBW / NEO_GRB - four/three channel driver type (defined in Adafruit_NeoPixel.h)
   #define NEOPIXEL_PIN    P1_24       // LED driving pin
