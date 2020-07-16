b5b0a0fcb19f9d9f0f86393ec53d2df860439173
xxorza <xxorza@gmail.com>
2020-07-16 14:13:38 -0700

    fan pwn tuned

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -376,8 +376,8 @@
  *
  * Define one or both of these to override the default 0-255 range.
  */
-#define FAN_MIN_PWM 25
-//#define FAN_MAX_PWM 128
+#define FAN_MIN_PWM 16
+#define FAN_MAX_PWM 160
 
 /**
  * FAST PWM FAN Settings
