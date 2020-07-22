fe00f7ef82db26f8b02d9d54292d86df0c4117ef
xxorza <xxorza@gmail.com>
2020-07-21 22:21:21 -0700

    tuned a bit

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -745,11 +745,11 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 200, 200, 25, 100 }
+#define DEFAULT_MAX_FEEDRATE          { 200, 200, 20, 100 }
 
 #define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
-  #define MAX_FEEDRATE_EDIT_VALUES    { 200, 200, 25, 100 } // ...or, set your own edit limits
+  #define MAX_FEEDRATE_EDIT_VALUES    { 200, 200, 20, 100 } // ...or, set your own edit limits
 #endif
 
 /**
@@ -758,11 +758,11 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 1200, 1200, 500, 5000 }
+#define DEFAULT_MAX_ACCELERATION      { 1200, 1200, 200, 5000 }
 
 #define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
-  #define MAX_ACCEL_EDIT_VALUES       { 1200, 1200, 500, 5000 } // ...or, set your own edit limits
+  #define MAX_ACCEL_EDIT_VALUES       { 1200, 1200, 200, 5000 } // ...or, set your own edit limits
 #endif
 
 /**
