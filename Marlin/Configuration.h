66080f7ebb9bb0029b00ecb2f6ba73b8bfda90aa
xxorza <xxorza@gmail.com>
2020-12-06 02:03:56 -0800

    steps per mm for e decreased

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -743,7 +743,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {100, 100, 400, 415}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 415 * 95 / 100 } 
 
 /**
  * Default Max Feed Rate (mm/s)
