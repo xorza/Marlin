607daa5342eb39682b36b385a021390240a2a5a0
xxorza <xxorza@gmail.com>
2021-01-11 19:48:29 -0800

    in dev

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1171,7 +1171,7 @@
   #define SD_PROCEDURE_DEPTH 1              // Increase if you need more nested M32 calls
 
   #define SD_FINISHED_STEPPERRELEASE true   // Disable steppers when SD Print is finished
-  #define SD_FINISHED_RELEASECOMMAND "G92 E0.0\nG1 E-15.0\nM140 S0\nM104 S0\nM107\nG27 P2\nM84\nG90"  // Use "M84XYE" to keep Z enabled so your bed stays in place
+  #define SD_FINISHED_RELEASECOMMAND "G92 E0.0\nM140 S0\nM104 S0\nM107\nG27 P2\nM84\nG90"  // Use "M84XYE" to keep Z enabled so your bed stays in place
 
   // Reverse SD sort to show "more recent" files first, according to the card's FAT.
   // Since the FAT gets out of order with usage, SDCARD_SORT_ALPHA is recommended.
