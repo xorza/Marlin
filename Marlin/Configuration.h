316ed83f32e08fb44ece7de3a1e150c5962f02c3
xxorza <xxorza@gmail.com>
2021-01-12 19:15:51 -0800

    clearance

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1031,10 +1031,10 @@
  */
 #define Z_CLEARANCE_DEPLOY_PROBE      3 // Z Clearance for Deploy/Stow
 #define Z_CLEARANCE_BETWEEN_PROBES    3 // Z Clearance between probe points
-#define Z_CLEARANCE_MULTI_PROBE     0.2 // Z Clearance between multiple probes
+#define Z_CLEARANCE_MULTI_PROBE     0.5 // Z Clearance between multiple probes
 #define Z_AFTER_PROBING              20 // Z position after probing is done
 
-#define Z_PROBE_LOW_POINT            -3 // Farthest distance below the trigger-point to go before stopping
+#define Z_PROBE_LOW_POINT            -2 // Farthest distance below the trigger-point to go before stopping
 
 // For M851 give a range for adjusting the Z probe offset
 #define Z_PROBE_OFFSET_RANGE_MIN -4
