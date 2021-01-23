b298464f77adc2d462487b27f8ef3431dae469f1
xxorza <xxorza@gmail.com>
2021-01-22 17:46:12 -0800

    acceleration tweaked

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -780,7 +780,7 @@
  */
 #define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  5000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_TRAVEL_ACCELERATION   1200    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk limits (mm/s)
