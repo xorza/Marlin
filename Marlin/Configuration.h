a7a4ad133f5ccab5022e4fadf7f96957efab6297
xxorza <xxorza@gmail.com>
2021-01-30 17:47:14 -0800

    reduced acceleration a bit

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -763,11 +763,11 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 2000, 2000, 500, 5000 }
+#define DEFAULT_MAX_ACCELERATION      { 2000, 2000, 300, 5000 }
 
 #define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
-  #define MAX_ACCEL_EDIT_VALUES       { 2000, 2000, 500, 5000 } // ...or, set your own edit limits
+  #define MAX_ACCEL_EDIT_VALUES       { 2000, 2000, 300, 5000 } // ...or, set your own edit limits
 #endif
 
 /**
