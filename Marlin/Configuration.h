f9e2ea731049e15a2c62c79a53153f9b9758baf9
xxorza <xxorza@gmail.com>
2021-03-16 20:05:14 -0700

    y speed increased

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -760,11 +760,11 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 200, 120, 20, 50 }
+#define DEFAULT_MAX_FEEDRATE          { 200, 200, 20, 50 }
 
 #define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
-  #define MAX_FEEDRATE_EDIT_VALUES    { 200, 120, 20, 50 } // ...or, set your own edit limits
+  #define MAX_FEEDRATE_EDIT_VALUES    { 200, 200, 20, 50 } // ...or, set your own edit limits
 #endif
 
 /**
