e21dbcd07238becb13dad8015381a95220bfa73e
xorza <xxorza@gmail.com>
2021-06-09 19:03:35 -0700

    reduced microstepping

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -815,7 +815,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT { 200, 200, 800, 400 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT { 50, 50, 100, 100 }
 
 /**
  * Default Max Feed Rate (mm/s)
